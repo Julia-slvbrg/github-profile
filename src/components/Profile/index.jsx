53e7b5c4205fe0ecc4aaf101f000ed3d39004ea1
Julia-slvbrg <julia.slvb@gmail.com>
2024-05-14 19:22:01 -0300

    chore: profile-component implemented and styled

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -1,8 +1,21 @@
-const Profile = () => {
+import PropTypes from 'prop-types';
+import styles from './Profile.module.css';
+
+const Profile = ({ userName }) => {
+
     return(
-        <>
-        </>
+        <header className={styles.header}>
+            <img className={styles.avatar} src={`https://github.com/${userName}.png`} alt="Foto do perfil do GitHub" />
+            <h1 className={styles.name}>
+                {userName}
+            </h1>
+        </header>
     )
 }
 
+Profile.propTypes = {
+    userName: PropTypes.string.isRequired
+};
+
 export default Profile
+
